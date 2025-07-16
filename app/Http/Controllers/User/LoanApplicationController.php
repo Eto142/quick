0af3fd31034db1766f4444f4860c821314641a6e
eto <eto.saviour80@gmail.com>
2025-07-16 13:47:22 +0100

    update

--- app/Http/Controllers/User/LoanApplicationController.php
+++ app/Http/Controllers/User/LoanApplicationController.php
@@ -1,8 +1,9 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+
 use App\Models\LoanApplication;
 use App\Models\Transaction;
 use Illuminate\Http\Request;
